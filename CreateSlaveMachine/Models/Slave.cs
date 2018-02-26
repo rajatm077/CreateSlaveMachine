a9cf8f1df4f87887d1eafb95a7f2ee2042a186ce
MalhotR1 <rajatmalhotra26@gmail.com>
2018-02-26 19:37:23 +0530

    Automating the process of adding slave machine to jenkins server

---
+++ CreateSlaveMachine/Models/Slave.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreateSlaveMachine.Models
+{
+    public enum OSType
+    {
+        Windows = 1,
+        Linux
+    }
+
+    public class Slave
+    {
+        [Required]
+        public string Machine { get; set; }
+
+        [Required]
+        public OSType OS { get; set; } = OSType.Windows;
+
+        [Required]
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+
+        
+        [Display(Name = "Remote Root Directory")]
+        public string Rrd { get; set; } //Remote root directory
+
+        public string Label { get; set; }
+        
+        
+        [DisplayName("# of executors")]
+        public int Threads { get; set; }
+
+    }
+}
